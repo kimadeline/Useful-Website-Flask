a168291d058cf043fecfa27583acc86f98d2e029
Kim-Adeline Miguel <kimiguel@microsoft.com>
2020-04-09 08:23:40 -0700

    Add auto refresh of the webpage

--- app.py
+++ app.py
@@ -1,19 +1,29 @@
 from flask import Flask, render_template, send_from_directory
+from livereload import Server
+
 import os
 import pytest
 import sys
 
 app = Flask(__name__)
 
-@app.route('/favicon.ico')
+
+@app.route("/favicon.ico")
 def favicon():
-    return send_from_directory(os.path.join(app.root_path, 'static'),
-                          'favicon.ico',mimetype='image/vnd.microsoft.icon')
+    return send_from_directory(
+        os.path.join(app.root_path, "static"),
+        "favicon.ico",
+        mimetype="image/vnd.microsoft.icon",
+    )
+
 
 @app.route("/")
 def home():
     myPlatform = sys.platform
-    return render_template("home.html")
+    return render_template("home.html", animal="dog")
+
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    server = Server(app.wsgi_app)
+    server.serve()
+    # app.run(debug=True)
